7d68d9d5077c77d4d6487cc6983310f250d51cf1
Argenis Ferrer <argenisf@gmail.com>
2019-05-03 17:33:27 +0200

    Initial Work Done

--- images/views.py
+++ images/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth import login as log_in
 from django.contrib.auth import logout as log_out
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from mixpanel import Mixpanel
+import datetime
 
 from images.forms import SignUpForm
 from images.forms import LoginForm
 
+mp = Mixpanel("f8bd7cddaf94642530004c3d0509691f")
 
 def index(request):
     """Return the logged in page, or the logged out page
@@ -14,7 +17,8 @@ def index(request):
     print('Index view!')
     if request.user.is_authenticated():
         return render(request, 'images/index-logged-in.html', {
-            'user': request.user
+            'user': request.user,
+            'email': request.user.email
         })
     else:
         return render(request, 'images/index-logged-out.html')
@@ -30,6 +34,18 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+            email = form.cleaned_data.get('email')
+            
+            mp_distinct_id = request.POST['mp_did']
+            now = datetime.datetime.utcnow()
+            
+            mp.track(mp_distinct_id, "Signup",{
+                "Username": username,
+                "Signup Date": now.strftime("%Y-%m-%dT%H:%M:%S")
+            });
+
+            mp.alias(email, mp_distinct_id)
+
             user = authenticate(username=username, password=raw_password)
             log_in(request, user)
             return HttpResponseRedirect('/')
@@ -49,6 +65,9 @@ def login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             log_in(request, user)
+
+            mp.track(request.user.email, "Login");
+
             return HttpResponseRedirect('/')
         else:
             return render(request, 'images/login.html', {
